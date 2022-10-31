a652b4ee8186c6b414ff83ed5f029f8c2c7ccc12
angelicodm <72331555+angelicodm@users.noreply.github.com>
2022-10-30 21:57:53 -0700

    Update main.cpp
    updated main.cpp to  follow sample more and implemented more of the search algorithm itself, still need the expand function and a way to check for repeated states

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <algorithm>
-
+#include "algorhythm.h"
 
 using namespace std;
 
@@ -11,12 +12,12 @@ vector<string> customPuzzle(string &input)
 	//cin.ignore(256, ' '); could potentially work if properly implemented but becomes more complicated with bigger puzzles
     getline(cin, input);
     remove(input.begin(), input.end(), ' '); //seamlessly removes all whitespace from input
-    vector<string> rows;
+    vector<string> matrix;
     for (int i = 0; i < 3; i++) //maxRows acts as the number of rows/columns in an 8-puzzle and should be able to be adjusted for 15 and 24 puzzles
     {
-        rows.push_back(string(1, input.at(i)));
+        matrix.push_back(string(1, input.at(i)));
     }
-    return rows;
+    return matrix;
 }
 
 /* THE GENERAL SEARCH ALGORITHM
@@ -30,38 +31,40 @@ vector<string> customPuzzle(string &input)
  * end
 */
 
-int main() {
+int main() 
+{
     vector<vector<string> > numberSequence; //vector of vectors acting as rows and columns
     string input;
     bool validInput = false;
     
-    while (!validInput) {
+    while (!validInput) 
+    {
         cout << "Enter 1 for default puzzle, 2 for custom puzzle: " << endl;
         cin >> input;
-        if (input == "1") {
+        if (input == "1") 
+        {
             numberSequence = { {"1", "2", "0"},
                        {"4", "5", "3"},
                        {"7", "8", "6"} };
             validInput = true;
         }
-        else if (input == "2") {
+        else if (input == "2") 
+        {
             cin.ignore();
             cout << "Enter your puzzle, use zero to represent the blank/empty space." << endl;
             cout << "Please enter only valid 8-puzzles (there should only be 3 single-digit entries per row)." << endl;
             cout << "Hit Enter/Return after inputting sequence of numbers:" << endl;
-          
             cout << "Enter the first row of numbers with a space in betweeen each number: " << endl;
             numberSequence.push_back(customPuzzle(input));
-          
             cout << "Enter the second row of numbers with a space in betweeen each number: " << endl;
             numberSequence.push_back(customPuzzle(input));
-          
             cout << "Enter the third row of numbers with a space in betweeen each number: " << endl;
             numberSequence.push_back(customPuzzle(input));
             
             validInput = true;
         }
-        else {
+        else 
+        {
             cout << "Invalid input, please input '1' or '2' to begin." << endl;
         }
     }
@@ -73,29 +76,46 @@ int main() {
     
     //decide between making multiple if statements depending on selected algorithm or separate helper function
 
-	//create the data structure of nodes
-    int queueSize;
+    slidePuzzle my_puzzle(numberSequence);
+    priority_queue <slidePuzzle> nodes; //should sort each node
+    
+    //find way to check for repeats
+    
+    nodes.push(my_puzzle); //creates the data structure of nodes
+    
+    int queueSize = nodes.size();
     int nodesExpanded = 0;
-    while(/* !empty */) //enter loop and ask if nodes is empty
+    
+    while(!nodes.empty()) //enter loop and ask if nodes is empty
     {
-        // pop off the head off nodes
-        if (/* goal state*/) //ask if its the goal state, if yes enter if statement and were done 
+        if (queueSize < nodes.size()) //updates queueSize
+        {
+            queueSize = nodes.size();
+        }
+        
+        slidePuzzle currentNode = nodes.top();
+        nodes.pop(); // pop off the head off nodes
+        
+        if (isGoal(currentNode)) //ask if its the goal state, if yes enter if statement and were done 
         {
             cout << "Solution found! " << endl;
-            
-            cout << "Solution depth was " << /* depth */<< endl;
+            currentNode.printPuzzle();
+            cout << "Solution depth was " << currentNode.getG() << endl;
             cout << "Number of nodes expanded: " << nodesExpanded << endl;
             cout << "Max queue size: " << queueSize << endl;
             
             return 0;
         }
         
-        cout << "The best state to is expand is: " << endl;
-        //if not goal state, creates all of current nodes children
+        cout << "The best state to is expand with a g(n) = " << currentNode.getG() << " and h(n) = " << currentNode.getH() << " is... " << endl;
+        
+        
+        currentNode.printPuzzle();
+        // EXPAND function: creates children based off of currentNode and selected algorithm ;if not goal state, creates all of current nodes children
         nodesExpanded++; //updates nodesExpanded and returns to top of the loop
         
     }
-    cout << "No solution found" << endl;
+    cout << "No solution could be found." << endl;
     cout << "Number of nodes expanded: " << nodesExpanded << endl;
     cout << "Max queue size: " << queueSize << endl;
     return 0;
